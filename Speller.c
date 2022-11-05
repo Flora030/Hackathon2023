37420776ef5ae163a4db6fbf9cf8a5d8d81a33fb
Kevin Zhong <kzhong2@cse220-73.cse.buffalo.edu>
2022-11-05 12:45:37 -0400

    text

--- Speller.c
+++ Speller.c
@@ -1,16 +1,23 @@
-<<<<<<< HEAD
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
 
-=======
-#include <stdio.h>
-
 const char* spellChecker(char* myString){
+    FILE    *textfile;
+    char    line[MAX_LINE_LENGTH];
+    
+    textfile = fopen("readme.txt", "r");
+    if(textfile == NULL)
+        return 1;
+    
+    while(fgets(line, MAX_LINE_LENGTH, textfile)){
+        printf(line);
+    }
+    
+    fclose(textfile);
 }
 
 int main(void){
     printf("%s",spellChecker(""));
 }
->>>>>>> 885d173994b249d25446acd6831a39a40cdd5ddf
