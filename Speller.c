ebb074b465dac5b4edeb86460d6a1200e8ab61b3
Dora <you@example.com>
2022-11-05 13:31:30 -0400

    6

--- Speller.c
+++ Speller.c
@@ -5,19 +5,6 @@
 #define MAX_LINE_LENGTH 1000
 
 const char* spellChecker(char* myString){
-    FILE    *textfile;
-    char    line[MAX_LINE_LENGTH];
-    textfile = fopen("readme.txt", "r");
-    if(textfile == NULL)
-        return 1;
-    while(fgets(line, MAX_LINE_LENGTH, textfile)){
-        
-    }
-    fclose(textfile);
-=======
-#define MAX_LINE_LENGTH 1000
-
-const char* spellChecker(char* myString){
     FILE *textfile;
     char line[MAX_LINE_LENGTH];
     textfile = fopen("readme.txt", "r");
@@ -147,10 +134,6 @@ typedef struct node
 node;
 
 node* HashTable[26]; //Table for all alphabets
-
-const char* spellChecker(char* myString){
-    
-}
 
 int main(void){
     printf("%s",spellChecker(""));
