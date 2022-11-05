2e8361aa470a7c1d0d184a9117abd6425f65b28f
Dora <you@example.com>
2022-11-05 13:01:16 -0400

    4

--- Speller.c
+++ Speller.c
@@ -2,22 +2,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#define MAX_LINE_LENGTH 1000
 
-<<<<<<< HEAD
 const char* spellChecker(char* myString){
     FILE    *textfile;
     char    line[MAX_LINE_LENGTH];
-    
     textfile = fopen("readme.txt", "r");
     if(textfile == NULL)
         return 1;
-    
     while(fgets(line, MAX_LINE_LENGTH, textfile)){
-        printf(line);
+        
     }
-    
     fclose(textfile);
-=======
 
 //Word in index of hash table
 typedef struct node
@@ -29,9 +25,10 @@ node;
 
 node* HashTable[26]; //Table for all alphabets
 
+
+
 const char* spellChecker(char* myString){
     
->>>>>>> e9250929422c7631586fcff553ae674864887323
 }
 
 int main(void){
