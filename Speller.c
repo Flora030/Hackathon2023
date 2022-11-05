e9250929422c7631586fcff553ae674864887323
Dora <you@example.com>
2022-11-05 12:43:08 -0400

    1

--- Speller.c
+++ Speller.c
@@ -1,16 +1,23 @@
-<<<<<<< HEAD
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
 
-=======
-#include <stdio.h>
+
+//Word in index of hash table
+typedef struct node
+{
+    char word[25];
+    struct node *next;
+}
+node;
+
+node* HashTable[26]; //Table for all alphabets
 
 const char* spellChecker(char* myString){
+    
 }
 
 int main(void){
     printf("%s",spellChecker(""));
 }
->>>>>>> 885d173994b249d25446acd6831a39a40cdd5ddf
